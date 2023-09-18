b486d71817dcf0adeb70fb8cdf61a43d561c9fb4
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-18 19:31:26 +0500

    export handlers from userHandler

--- user-service/app/handler.js
+++ user-service/app/handler.js
@@ -1,13 +1,1 @@
-export const Signup = async (event) => {
-	return {
-		statusCode: 200,
-		headers: {
-			"Access-Control-Allow-Origin": "8",
-		},
-		body: JSON.stringify({
-			message: "Response from signup functionnnnnnnnn",
-			input: event,
-			data: {},
-		}),
-	};
-};
+export * from "./handlers/userHandler.js";

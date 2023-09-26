2ad38bb68acf5cc92e3a648aa19e18a46da86884
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-26 15:45:13 +0500

    updates verification code for user

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -1,31 +1,38 @@
 import { UserModel } from "../models/UserModel.ts";
-import { DBClient } from "../utility/databaseClient";
+import { DBOperation } from "./dbOperation.js";
 
-export class UserRepository {
-	constructor() {}
+export class UserRepository extends DBOperation {
+	constructor() {
+		super();
+	}
 
 	async createAccount({ phone, email, password, salt, userType }: UserModel) {
-		const client = await DBClient();
-		await client.connect();
 		const queryString =
 			"INSERT INTO users(phone,email,password,salt,user_type) VALUES($1,$2,$3,$4,$5) RETURNING *";
 		const values = [phone, email, password, salt, userType];
-		const result = await client.query(queryString, values);
-		await client.end();
+		const result = await this.executeQuery(queryString, values);
 		if (result.rowCount > 0) {
 			return result.rows[0] as UserModel;
 		}
 	}
 	async findAccount(email: string) {
-		const client = await DBClient();
-		await client.connect();
 		const queryString =
-			"SELECT user_id, email, password, phone, salt FROM users WHERE email = $1";
+			"SELECT user_id, email, password, phone, salt, verification_code , expiry FROM users WHERE email = $1";
 		const values = [email];
-		const result = await client.query(queryString, values);
-		await client.end();
+		const result = await this.executeQuery(queryString, values);
 		if (result.rowCount < 1) {
 			throw new Error("user does not exist with provided email id!");
+		}
+		return result.rows[0] as UserModel;
+	}
+
+	async updateVerificationCode(userId: string, code: number, expiry: Date) {
+		const queryString =
+			"UPDATE users SET verification_code=$1, expiry=$2 WHERE user_id = $3 RETURNING *";
+		const values = [code, expiry, userId];
+		const result = await this.executeQuery(queryString, values);
+		if (result.rowCount < 1) {
+			throw new Error("user does not exist with provided userID!");
 		}
 		return result.rows[0] as UserModel;
 	}

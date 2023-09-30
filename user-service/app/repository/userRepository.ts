b815fae169d182d404ce2e9d3bc18f0e8ce401c5
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-30 06:36:45 +0500

    adds function to update user info and insert user addresses in db

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -1,4 +1,5 @@
 import { UserModel } from "../models/UserModel.ts";
+import { ProfileInput } from "../models/dto/ProfileInput.js";
 import { DBOperation } from "./dbOperation.js";
 
 export class UserRepository extends DBOperation {
@@ -26,7 +27,7 @@ export class UserRepository extends DBOperation {
 		return result.rows[0] as UserModel;
 	}
 
-	async updateVerificationCode(userId: string, code: number, expiry: Date) {
+	async updateVerificationCode(userId: number, code: number, expiry: Date) {
 		const queryString =
 			"UPDATE users SET verification_code=$1, expiry=$2 WHERE user_id = $3 AND verified=FALSE RETURNING *";
 		const values = [code, expiry, userId];
@@ -37,7 +38,7 @@ export class UserRepository extends DBOperation {
 		return result.rows[0] as UserModel;
 	}
 
-	async updateVerifyUser(userId: string) {
+	async updateVerifyUser(userId: number) {
 		const queryString =
 			"UPDATE users SET verified=TRUE WHERE user_id = $1 AND verified=FALSE RETURNING *";
 		const values = [userId];
@@ -46,5 +47,53 @@ export class UserRepository extends DBOperation {
 			throw new Error("User already verified");
 		}
 		return result.rows[0] as UserModel;
+	}
+
+	async updateUser(
+		userId: number,
+		firstName: string,
+		lastName: string,
+		userType: string
+	) {
+		const queryString =
+			"UPDATE users SET first_name= $1, last_name=$2, user_type=$3 WHERE user_id = $4 RETURNING *";
+		const values = [firstName, lastName, userType, userId];
+		const result = await this.executeQuery(queryString, values);
+		if (result.rowCount < 1) {
+			throw new Error("Error while updating user");
+		}
+		return result.rows[0] as UserModel;
+	}
+
+	async createUserProfile(
+		userId: number,
+		{
+			firstName,
+			lastName,
+			userType,
+			address: { addressLine1, addressLine2, city, postCode, country },
+		}: ProfileInput
+	) {
+		const updatedUser = await this.updateUser(
+			userId,
+			firstName,
+			lastName,
+			userType
+		);
+		const queryString =
+			"INSERT into  address (user_id, address_line1 ,address_line2, city, post_code, country)  VALUES ($1,$2,$3,$4,$5 ,$6) RETURNING *";
+		const values = [
+			userId,
+			addressLine1,
+			addressLine2,
+			city,
+			postCode,
+			country,
+		];
+		const result = await this.executeQuery(queryString, values);
+		if (result.rowCount < 1) {
+			throw new Error("User already verified");
+		}
+		return { updatedUser, result };
 	}
 }

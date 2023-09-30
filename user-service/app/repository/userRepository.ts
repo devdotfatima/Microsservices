69a12b40c72b71317d99b75d6f3df84993f6631b
Fatima Ali <fatimaaliforwork@gmail.com>
2023-09-30 13:07:16 +0500

    adds function to get user info & addresses from db

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -1,3 +1,4 @@
+import { AddressModel } from "../models/AddressModel.js";
 import { UserModel } from "../models/UserModel.ts";
 import { ProfileInput } from "../models/dto/ProfileInput.js";
 import { DBOperation } from "./dbOperation.js";
@@ -16,6 +17,7 @@ export class UserRepository extends DBOperation {
 			return result.rows[0] as UserModel;
 		}
 	}
+
 	async findAccount(email: string) {
 		const queryString =
 			"SELECT user_id, email, password, phone, salt, verification_code , expiry FROM users WHERE email = $1";
@@ -74,12 +76,7 @@ export class UserRepository extends DBOperation {
 			address: { addressLine1, addressLine2, city, postCode, country },
 		}: ProfileInput
 	) {
-		const updatedUser = await this.updateUser(
-			userId,
-			firstName,
-			lastName,
-			userType
-		);
+		await this.updateUser(userId, firstName, lastName, userType);
 		const queryString =
 			"INSERT into  address (user_id, address_line1 ,address_line2, city, post_code, country)  VALUES ($1,$2,$3,$4,$5 ,$6) RETURNING *";
 		const values = [
@@ -92,8 +89,30 @@ export class UserRepository extends DBOperation {
 		];
 		const result = await this.executeQuery(queryString, values);
 		if (result.rowCount < 1) {
-			throw new Error("User already verified");
+			throw new Error("Error while creating profile");
 		}
-		return { updatedUser, result };
+		return result.rows[0] as AddressModel;
+	}
+
+	async getUserProfile(userId: number) {
+		const profileQuery =
+			"SELECT first_name, last_name, email, phone, user_type, verified FROM users WHERE user_id = $1";
+		const profileValues = [userId];
+		const profileResult = await this.executeQuery(profileQuery, profileValues);
+		if (profileResult.rowCount < 1) {
+			throw new Error("user does not exist with provided user id!");
+		}
+		const userProfile = profileResult.rows[0] as UserModel;
+
+		const addressQuery =
+			"SELECT id,address_line1, address_line2, city, post_code, country FROM address WHERE user_id = $1";
+		const addressValues = [userId];
+		const addressResult = await this.executeQuery(addressQuery, addressValues);
+
+		if (addressResult.rowCount > 0) {
+			userProfile.address = addressResult.rows as AddressModel[];
+		}
+
+		return userProfile;
 	}
 }

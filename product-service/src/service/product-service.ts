3af22736b7312beded0c6b3ef2c637b745215859
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-09 15:00:05 +0500

    implements createProduct api endpoint

--- product-service/src/service/product-service.ts
+++ product-service/src/service/product-service.ts
@@ -1,72 +1,9 @@
+import { plainToClass } from "class-transformer";
 import { APIGatewayEvent } from "aws-lambda";
 import { ProductRepository } from "../repository/product-repository";
-import { ErrorResponse, SucessResponse } from "../utility/response";
-// import { plainToClass } from "class-transformer";
-// import { AppValidationError } from "../utility/errors";
-// import { ProductInput } from "../dto/product-input";
-// import { CategoryRepository } from "../repository/category-repository";
-
-// export class ProductService {
-//   _repository: ProductRepository;
-//   constructor(repository: ProductRepository) {
-//     this._repository = repository;
-//   }
-
-//   async createProduct(event: APIGatewayEvent) {
-//     const input = plainToClass(ProductInput, JSON.parse(event.body!));
-//     const error = await AppValidationError(input);
-//     if (error) return ErrorResponse(404, error);
-
-//     const data = await this._repository.createProduct(input);
-
-//     await new CategoryRepository().addItem({
-//       id: input.category_id,
-//       products: [data._id],
-//     });
-//     return SucessResponse(data);
-//   }
-
-//   async getProducts(event: APIGatewayEvent) {
-//     const data = await this._repository.getAllProducts();
-//     return SucessResponse(data);
-//   }
-
-//   async getProduct(event: APIGatewayEvent) {
-//     const productId = event.pathParameters?.id;
-//     if (!productId) return ErrorResponse(403, "please provide product id");
-
-//     const data = await this._repository.getProductById(productId);
-//     return SucessResponse(data);
-//   }
-
-//   async editProduct(event: APIGatewayEvent) {
-//     const productId = event.pathParameters?.id;
-//     if (!productId) return ErrorResponse(403, "please provide product id");
-
-//     const input = plainToClass(ProductInput, JSON.parse(event.body!));
-//     const error = await AppValidationError(input);
-//     if (error) return ErrorResponse(404, error);
-
-//     input.id = productId;
-//     const data = await this._repository.updateProduct(input);
-
-//     return SucessResponse(data);
-//   }
-
-//   async deleteProduct(event: APIGatewayEvent) {
-//     const productId = event.pathParameters?.id;
-//     if (!productId) return ErrorResponse(403, "please provide product id");
-
-//     const { category_id, deleteResult } = await this._repository.deleteProduct(
-//       productId
-//     );
-//     await new CategoryRepository().addItem({
-//       id: category_id,
-//       products: [productId],
-//     });
-//     return SucessResponse(deleteResult);
-//   }
-// }
+import { ErrorResponse, SuccessResponse } from "../utility/response";
+import { ProductInput } from "../models/dto/product-input";
+import { AppValidationError } from "../utility/errors";
 
 export class ProductService {
 	repository: ProductRepository;
@@ -74,18 +11,28 @@ export class ProductService {
 		this.repository = repository;
 	}
 	async createProduct(event: APIGatewayEvent) {
-		return SucessResponse({ msg: "Product Created" });
+		try {
+			const input = plainToClass(ProductInput, JSON.parse(event.body!));
+			const error = await AppValidationError(input);
+			if (error) return ErrorResponse(404, error);
+			const data = await this.repository.createProduct(input);
+
+			return SuccessResponse(data);
+		} catch (error) {
+			console.log(error);
+			return ErrorResponse(500, error);
+		}
 	}
 	async getProducts(event: APIGatewayEvent) {
-		return SucessResponse({ msg: "Products Returned" });
+		return SuccessResponse({ msg: "Products Returned" });
 	}
 	async getProduct(event: APIGatewayEvent) {
-		return SucessResponse({ msg: "Product Returned" });
+		return SuccessResponse({ msg: "Product Returned" });
 	}
 	async editProduct(event: APIGatewayEvent) {
-		return SucessResponse({ msg: "Products Edited" });
+		return SuccessResponse({ msg: "Products Edited" });
 	}
 	async deleteProduct(event: APIGatewayEvent) {
-		return SucessResponse({ msg: "Products Deleted" });
+		return SuccessResponse({ msg: "Products Deleted" });
 	}
 }

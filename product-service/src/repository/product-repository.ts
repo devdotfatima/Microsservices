862c06651e2e0088082717eb9e8d15550ab3624e
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-09 14:57:55 +0500

    adds createproduct function to add new product data in db

--- product-service/src/repository/product-repository.ts
+++ product-service/src/repository/product-repository.ts
@@ -1,3 +1,23 @@
+import { ProductDoc, products } from "../models/product-model";
+import { ProductInput } from "../models/dto/product-input";
+
 export class ProductRepository {
 	constructor() {}
+
+	async createProduct({
+		name,
+		description,
+		price,
+		category_id,
+		image_url,
+	}: ProductInput): Promise<ProductDoc> {
+		return products.create({
+			name,
+			description,
+			price,
+			category_id,
+			image_url,
+			availability: true,
+		});
+	}
 }

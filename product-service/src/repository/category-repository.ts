1445701d717ed6c565d6edbd7d468953e34b06bf
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-17 21:08:15 +0500

    creates  category repository class

---
+++ product-service/src/repository/category-repository.ts
@@ -0,0 +1,6 @@
+import { AddItemInput, CategoryInput } from "../models/dto/category-input";
+import { CategoryDoc, categories } from "../models";
+
+export class CategoryRepository {
+	constructor() {}
+}

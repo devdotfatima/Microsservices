ae466fe2e901a14ea67271b40a5c0896298bb825
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-17 21:10:35 +0500

    adds createCategory function to add categories and subcategory in database

--- product-service/src/repository/category-repository.ts
+++ product-service/src/repository/category-repository.ts
@@ -3,4 +3,30 @@ import { CategoryDoc, categories } from "../models";
 
 export class CategoryRepository {
 	constructor() {}
+
+	async createCategory({ name, parentId, imageUrl }: CategoryInput) {
+		// create a new category
+		const newCategory = await categories.create({
+			name,
+			parentId,
+			subCategory: [],
+			products: [],
+			imageUrl,
+		});
+		// parent id exist
+		// update parent category with the new sub category id
+		if (parentId) {
+			const parentCategory = (await categories.findById(
+				parentId
+			)) as CategoryDoc;
+			parentCategory.subCategories = [
+				...parentCategory.subCategories,
+				newCategory,
+			];
+			await parentCategory.save();
+		}
+		// return newly create category
+
+		return newCategory;
+	}
 }

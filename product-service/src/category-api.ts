5e0547fed0a1746c9aee6306c8b9b55b23de9455
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-17 20:45:20 +0500

    creates handler function for category service

---
+++ product-service/src/category-api.ts
@@ -0,0 +1,34 @@
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ErrorResponse } from "./utility/response";
+
+import "./utility";
+import { CategoryRepository } from "./repository/category-repository";
+import { CategoryService } from "./service/category-service";
+const service = new CategoryService(new CategoryRepository());
+
+export const handler = async (
+	event: APIGatewayEvent,
+	context: Context
+): Promise<APIGatewayProxyResult> => {
+	const isRoot = event.pathParameters === null;
+
+	switch (event.httpMethod.toLowerCase()) {
+		case "post":
+			if (isRoot) {
+				return service.createCategory(event);
+			}
+			break;
+		case "get":
+			return isRoot ? service.getCategories(event) : service.getCategory(event);
+		case "put":
+			if (!isRoot) {
+				return service.editCategory(event);
+			}
+		case "delete":
+			if (!isRoot) {
+				return service.deleteCategory(event);
+			}
+	}
+
+	return service.ResponseWithError(event);
+};

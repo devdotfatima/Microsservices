e4b1f634b608448871e79bd2200fdd6da64efa64
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-17 17:19:49 +0500

    Adds S3 bucket to the stack and integrates Image api Handler to store files in bucket and return back the url

--- product-service/lib/service-stack.ts
+++ product-service/lib/service-stack.ts
@@ -1,3 +1,4 @@
+import { Duration } from "aws-cdk-lib";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import {
 	NodejsFunction,
@@ -7,11 +8,14 @@ import { Construct } from "constructs";
 import { join } from "path";
 
 interface ServiceProps {
-	bucket?: any;
+	bucket: string;
 }
 
 export class ServiceStack extends Construct {
 	public readonly productService: NodejsFunction;
+	public readonly categoryService: NodejsFunction;
+	public readonly dealsService: NodejsFunction;
+	public readonly imageService: NodejsFunction;
 
 	constructor(scope: Construct, id: string, props: ServiceProps) {
 		super(scope, id);
@@ -21,13 +25,29 @@ export class ServiceStack extends Construct {
 				externalModules: ["aws-sdk"],
 			},
 			environment: {
-				BUCKET_NAME: "OUR_BUCKET_ARN",
+				BUCKET_NAME: props.bucket,
 			},
 			runtime: Runtime.NODEJS_16_X,
+			timeout: Duration.seconds(10),
 		};
 
 		this.productService = new NodejsFunction(this, "productLambda", {
-			entry: join(__dirname, "/../src/index.ts"),
+			entry: join(__dirname, "/../src/product-api.ts"),
+			...nodeJsFunctionProps,
+		});
+
+		this.categoryService = new NodejsFunction(this, "categoryLambda", {
+			entry: join(__dirname, "/../src/category-api.ts"),
+			...nodeJsFunctionProps,
+		});
+
+		this.dealsService = new NodejsFunction(this, "dealsLambda", {
+			entry: join(__dirname, "/../src/deals-api.ts"),
+			...nodeJsFunctionProps,
+		});
+
+		this.imageService = new NodejsFunction(this, "imageUploadLambda", {
+			entry: join(__dirname, "/../src/image-api.ts"),
 			...nodeJsFunctionProps,
 		});
 	}

45d516c66d11fb247e9dc6eddd7d251bc0fe02bc
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-17 21:24:26 +0500

    creates CategoryService & implements createCategory api endpoint

---
+++ product-service/src/service/category-service.ts
@@ -0,0 +1,25 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { CategoryRepository } from "../repository/category-repository";
+import { ErrorResponse, SuccessResponse } from "../utility/response";
+import { plainToClass } from "class-transformer";
+import { AppValidationError } from "../utility/errors";
+import { CategoryInput } from "../models/dto/category-input";
+
+export class CategoryService {
+	repository: CategoryRepository;
+	constructor(repository: CategoryRepository) {
+		this.repository = repository;
+	}
+
+	async ResponseWithError(event: APIGatewayEvent) {
+		return ErrorResponse(404, new Error("method not allowed!"));
+	}
+
+	async createCategory(event: APIGatewayEvent) {
+		const input = plainToClass(CategoryInput, JSON.parse(event.body!));
+		const error = await AppValidationError(input);
+		if (error) return ErrorResponse(404, error);
+		const data = await this.repository.createCategory(input);
+		return SuccessResponse(data);
+	}
+}

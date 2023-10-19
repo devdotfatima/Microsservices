f85fcd7710a09952820e4fbba466eaf1e7c9c13d
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-19 12:02:58 +0500

    Adds findShoppingCart func in cartRepository to return cart based on userID

--- user-service/app/repository/cartRepository.ts
+++ user-service/app/repository/cartRepository.ts
@@ -1,7 +1,20 @@
+import { CartItemModel } from "../models/CartItemModel";
+import { ShoppingCartModel } from "../models/ShoppingCartModel";
 import { DBOperation } from "./dbOperation";
 
 export class CartRepository extends DBOperation {
 	constructor() {
 		super();
+	}
+
+	async findShoppingCart(userId: number) {
+		const queryString =
+			"SELECT cart_id, user_id from shopping_carts WHERE user_id=$1";
+		const values = [userId];
+		const result = await this.executeQuery(queryString, values);
+		if (result.rowCount < 1) {
+			return false;
+		}
+		return result.rows[0] as ShoppingCartModel;
 	}
 }

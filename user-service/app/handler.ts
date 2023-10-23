5e545ddebb893f845b79e8f5bbf1423cd97bf3b2
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-24 03:09:49 +0500

    nit

--- user-service/app/handler.ts
+++ user-service/app/handler.ts
@@ -1,4 +1,7 @@
 import "reflect-metadata";
+import dotenv from "dotenv";
+dotenv.config();
 
 export * from "./handlers/userHandler";
 export * from "./handlers/cartHandler";
+export * from "./handlers/orderHandler";

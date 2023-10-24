c916f991a5f3ab01b65b39b53d0632735d54f7e6
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-24 14:25:30 +0500

    Adds updateUserPayment func in userRepositiry to update the user payment and stripeID

--- user-service/app/repository/userRepository.ts
+++ user-service/app/repository/userRepository.ts
@@ -13,9 +13,10 @@ export class UserRepository extends DBOperation {
 			"INSERT INTO users(phone,email,password,salt,user_type) VALUES($1,$2,$3,$4,$5) RETURNING *";
 		const values = [phone, email, password, salt, userType];
 		const result = await this.executeQuery(queryString, values);
-		if (result.rowCount > 0) {
-			return result.rows[0] as UserModel;
+		if (result.rowCount < 1) {
+			throw new Error("user does not exist with provided email id!");
 		}
+		return result.rows[0] as UserModel;
 	}
 
 	async findAccount(email: string) {
@@ -140,5 +141,25 @@ export class UserRepository extends DBOperation {
 		if (addressResult.rowCount < 1) {
 			throw new Error("Error while updating profile");
 		}
+		return true;
+	}
+
+	async updateUserPayment({
+		userId,
+		paymentId,
+		customerId,
+	}: {
+		userId: number;
+		paymentId: string;
+		customerId: string;
+	}) {
+		const queryString =
+			"UPDATE users SET stripe_id=$1, payment_id=$2 WHERE user_id=$3 RETURNING * ";
+		const values = [customerId, paymentId, userId];
+		const result = await this.executeQuery(queryString, values);
+		if (result.rowCount < 1) {
+			throw new Error("error While updating user payment!");
+		}
+		return result.rows[0] as UserModel;
 	}
 }

28cd3c4572a2c19c0f99e8fe14ca47e20bb5afe5
Fatima Ali <fatimaaliforwork@gmail.com>
2023-10-31 14:19:56 +0500

    updates product service url

--- user-service/app/message-queue/index.ts
+++ user-service/app/message-queue/index.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const PRODUCT_SERVICE_URL = "http://127.0.0.1:3000/products-queue";
+// const PRODUCT_SERVICE_URL = "http://127.0.0.1:3000/products-queue";
 // "https://kg4ae0v376.execute-api.eu-central-1.amazonaws.com/prod/products-queue"; //"; // it will be come from process.env
-
+const PRODUCT_SERVICE_URL =
+	"https://7ziu3v9n8d.execute-api.us-east-1.amazonaws.com/prod/products-queue";
 export const PullData = async (requestData: Record<string, unknown>) => {
 	return axios.post(PRODUCT_SERVICE_URL, requestData);
 };

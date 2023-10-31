4398c223f9661793d43c6d4e923b9916fe2db1a1
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-01 01:29:55 +0500

    update transaction service stack to add multiple lambda handlers

--- transaction-service/lib/transaction-service-stack.ts
+++ transaction-service/lib/transaction-service-stack.ts
@@ -1,15 +1,11 @@
 import * as cdk from "aws-cdk-lib";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
-import {
-	NodejsFunction,
-	NodejsFunctionProps,
-} from "aws-cdk-lib/aws-lambda-nodejs";
-import { Topic, SubscriptionFilter } from "aws-cdk-lib/aws-sns";
+import { SubscriptionFilter, Topic } from "aws-cdk-lib/aws-sns";
 import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Queue } from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
-import { join } from "path";
+import { ServiceStack } from "./service-stack";
+import { ApiGatewayStack } from "./api-gateway-stack";
 
 export class TransactionServiceStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,7 +17,7 @@ export class TransactionServiceStack extends cdk.Stack {
 
 		const orderTopic = Topic.fromTopicArn(
 			this,
-			"order-consume-topic",
+			"order_consume-Topic",
 			cdk.Fn.importValue("customer-topic")
 		);
 
@@ -36,24 +32,15 @@ export class TransactionServiceStack extends cdk.Stack {
 			})
 		);
 
-		// handler
-		const nodeJsFunctionProps: NodejsFunctionProps = {
-			bundling: {
-				externalModules: ["aws-sdk"],
-			},
-			runtime: Runtime.NODEJS_16_X,
-			timeout: cdk.Duration.seconds(10),
-		};
+		const { createOrder, getOrder, getOrders, getTransaction } =
+			new ServiceStack(this, "transaction-service", {});
+		createOrder.addEventSource(new SqsEventSource(orderQueue));
 
-		const createOrderHandler = new NodejsFunction(
-			this,
-			"create-order-handler",
-			{
-				entry: join(__dirname, "/../src/order/create.ts"),
-				...nodeJsFunctionProps,
-			}
-		);
-
-		createOrderHandler.addEventSource(new SqsEventSource(orderQueue));
+		new ApiGatewayStack(this, "transaction-api-gateway", {
+			createOrder,
+			getOrders,
+			getOrder,
+			getTransaction,
+		});
 	}
 }

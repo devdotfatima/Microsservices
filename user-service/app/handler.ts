11032a625f8d164886058ff04f67d563751d1824
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-02 14:37:36 +0500

    adds seller handler in main handler file

--- user-service/app/handler.ts
+++ user-service/app/handler.ts
@@ -5,3 +5,4 @@ dotenv.config();
 export * from "./handlers/userHandler";
 export * from "./handlers/cartHandler";
 export * from "./handlers/orderHandler";
+export * from "./handlers/sellerHandler";

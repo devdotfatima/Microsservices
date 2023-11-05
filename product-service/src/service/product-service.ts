5257e1b64c86a8d8f9329d078a75a387e99ad949
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-05 16:48:54 +0500

    Adds try catch block and implements authorizartion

--- product-service/src/service/product-service.ts
+++ product-service/src/service/product-service.ts
@@ -14,6 +14,12 @@ export class ProductService {
 		this.repository = repository;
 	}
 
+	async authorisedUser(user_id: number, productId: string) {
+		const product = await this.repository.getProductById(productId);
+		if (!product) return false;
+		return Number(user_id) === Number(product.seller_id);
+	}
+
 	async createProduct(event: APIGatewayEvent) {
 		try {
 			const token = event.headers.Authorization;
@@ -48,44 +54,123 @@ export class ProductService {
 	}
 
 	async getProducts(event: APIGatewayEvent) {
-		const data = await this.repository.getAllProducts();
-		return SuccessResponse(data);
+		try {
+			const data = await this.repository.getAllProducts();
+			return SuccessResponse(data);
+		} catch (error) {
+			console.log(error);
+			return ErrorResponse(500, error);
+		}
+	}
+
+	async getSellerProducts(event: APIGatewayEvent) {
+		try {
+			const token = event.headers.Authorization;
+			const user = await AuthUser(token);
+			if (!user) return ErrorResponse(403, "authorization failed");
+
+			if (user.user_type.toUpperCase() !== "SELLER") {
+				return ErrorResponse(
+					403,
+					"you need to join the seller program to manage product"
+				);
+			}
+
+			const data = await this.repository.getAllSellerProducts(user.user_id);
+			return SuccessResponse(data);
+		} catch (error) {
+			console.log(error);
+			return ErrorResponse(500, error);
+		}
 	}
 
 	async getProduct(event: APIGatewayEvent) {
-		const productId = event.pathParameters?.id;
-		if (!productId) return ErrorResponse(403, "please provide product id");
+		try {
+			const productId = event.pathParameters?.id;
+			if (!productId) return ErrorResponse(403, "please provide product id");
 
-		const data = await this.repository.getProductById(productId);
-		return SuccessResponse(data);
+			const data = await this.repository.getProductById(productId);
+			return SuccessResponse(data);
+		} catch (error) {
+			console.log(error);
+			return ErrorResponse(500, error);
+		}
 	}
 
 	async editProduct(event: APIGatewayEvent) {
-		const productId = event.pathParameters?.id;
-		if (!productId) return ErrorResponse(403, "please provide product id");
+		try {
+			const token = event.headers.Authorization;
+			const user = await AuthUser(token);
+			if (!user) return ErrorResponse(403, "authorization failed");
 
-		const input = plainToClass(ProductInput, JSON.parse(event.body!));
-		const error = await AppValidationError(input);
-		if (error) return ErrorResponse(404, error);
+			if (user.user_type.toUpperCase() !== "SELLER") {
+				return ErrorResponse(
+					403,
+					"you need to join the seller program to manage product"
+				);
+			}
 
-		input.id = productId;
-		const data = await this.repository.updateProduct(input);
+			const productId = event.pathParameters?.id;
+			if (!productId) return ErrorResponse(403, "please provide product id");
 
-		return SuccessResponse(data);
+			const input = plainToClass(ProductInput, JSON.parse(event.body!));
+			const error = await AppValidationError(input);
+			if (error) return ErrorResponse(404, error);
+
+			const isAuthorised = await this.authorisedUser(user.user_id, productId);
+			if (!isAuthorised)
+				return ErrorResponse(
+					403,
+					"you are not authorised to edit this product"
+				);
+
+			input.id = productId;
+			const data = await this.repository.updateProduct(input);
+
+			return SuccessResponse(data);
+		} catch (error) {
+			console.log(error);
+			return ErrorResponse(500, error);
+		}
 	}
 
 	async deleteProduct(event: APIGatewayEvent) {
-		const productId = event.pathParameters?.id;
-		if (!productId) return ErrorResponse(403, "please provide product id");
+		try {
+			const token = event.headers.Authorization;
+			const user = await AuthUser(token);
+			if (!user) return ErrorResponse(403, "authorization failed");
 
-		const { category_id, deleteResult } = await this.repository.deleteProduct(
-			productId
-		);
-		await new CategoryRepository().addItem({
-			id: category_id,
-			products: [productId],
-		});
-		return SuccessResponse(deleteResult);
+			if (user.user_type.toUpperCase() !== "SELLER") {
+				return ErrorResponse(
+					403,
+					"you need to join the seller program to manage products"
+				);
+			}
+
+			const productId = event.pathParameters?.id;
+			if (!productId) return ErrorResponse(403, "please provide product id");
+
+			const isAuthorised = await this.authorisedUser(user.user_id, productId);
+			if (!isAuthorised)
+				return ErrorResponse(
+					403,
+					"you are not authorised to delete this product"
+				);
+
+			const { category_id, deleteResult } = await this.repository.deleteProduct(
+				productId
+			);
+
+			await new CategoryRepository().removeItem({
+				id: category_id,
+				products: [productId],
+			});
+
+			return SuccessResponse(deleteResult);
+		} catch (error) {
+			console.log(error);
+			return ErrorResponse(500, error);
+		}
 	}
 
 	// http calls // later stage we will convert this thing to RPC & Queue

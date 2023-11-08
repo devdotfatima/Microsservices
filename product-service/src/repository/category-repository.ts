08926618e39a83d2f3c41a59156e4b930f4afcad
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-08 14:14:35 +0500

    updates the catgory repository to add seller id wjile creating catgeory and add a function to return all categories created by same seller

--- product-service/src/repository/category-repository.ts
+++ product-service/src/repository/category-repository.ts
@@ -4,7 +4,7 @@ import { CategoryDoc, categories } from "../models";
 export class CategoryRepository {
 	constructor() {}
 
-	async createCategory({ name, parentId, imageUrl }: CategoryInput) {
+	async createCategory({ name, parentId, imageUrl, seller_id }: CategoryInput) {
 		// create a new category
 		const newCategory = await categories.create({
 			name,
@@ -12,6 +12,7 @@ export class CategoryRepository {
 			subCategory: [],
 			products: [],
 			imageUrl,
+			seller_id,
 		});
 		// parent id exist
 		// update parent category with the new sub category id
@@ -43,6 +44,17 @@ export class CategoryRepository {
 			})
 			.skip(offset)
 			.limit(perPage ? perPage : 100);
+	}
+
+	async getAllSellerCategories(seller_id: number) {
+		return categories.find({ seller_id, parentId: null }).populate({
+			path: "subCategories",
+			model: "categories",
+			populate: {
+				path: "subCategories",
+				model: "categories",
+			},
+		});
 	}
 
 	async getTopCategories() {
@@ -91,11 +103,17 @@ export class CategoryRepository {
 	}
 
 	async removeItem({ id, products }: AddItemInput) {
-		let category = (await categories.findById(id)) as CategoryDoc;
-		const excludeProducts = category.products.filter(
-			(item) => !products.includes(item)
+		const updatedCategory = await categories.findByIdAndUpdate(
+			id,
+			{ $pull: { products: products[0] } },
+			{ new: true } // To get the updated document
 		);
-		category.products = excludeProducts;
-		return category.save();
+
+		if (!updatedCategory) {
+			throw new Error("Category not found");
+		}
+		console.log(updatedCategory);
+		return updatedCategory;
+		//
 	}
 }

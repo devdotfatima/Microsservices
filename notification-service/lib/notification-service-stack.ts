fe993dba78bab60f96e37ea3e1a371fd23a1187b
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-08 18:41:39 +0500

    creates notification service stack

--- notification-service/lib/notification-service-stack.ts
+++ notification-service/lib/notification-service-stack.ts
@@ -1,16 +1,52 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { SubscriptionFilter, Topic } from "aws-cdk-lib/aws-sns";
+import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
+import { ServiceStack } from "./service-stack";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NotificationServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		const emailQueue = new Queue(this, "email_queue", {
+			visibilityTimeout: cdk.Duration.seconds(120),
+		});
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NotificationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		const otpQueue = new Queue(this, "otp_queue", {
+			visibilityTimeout: cdk.Duration.seconds(120),
+		});
+
+		const topic = new Topic(this, "notification_topic");
+		this.addSubscription(topic, emailQueue, ["customer_email"]);
+		this.addSubscription(topic, otpQueue, ["customer_otp"]);
+		const { emailHandler, otpHandler } = new ServiceStack(
+			this,
+			"notification_service",
+			{}
+		);
+		emailHandler.addEventSource(new SqsEventSource(emailQueue));
+
+		otpHandler.addEventSource(new SqsEventSource(otpQueue));
+
+		new cdk.CfnOutput(this, "NotificationTopic", {
+			value: topic.topicArn,
+			exportName: "notifySvcArn",
+		});
+	}
+
+	addSubscription(topic: Topic, queue: Queue, allowlist: string[]) {
+		topic.addSubscription(
+			new SqsSubscription(queue, {
+				rawMessageDelivery: true,
+				filterPolicy: {
+					actionType: SubscriptionFilter.stringFilter({
+						allowlist,
+					}),
+				},
+			})
+		);
+	}
 }

a9f1e08e342fe2e6aa20240bc47d071e374c7cf4
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-08 18:32:42 +0500

    creates basic handlers for email and otp and export them in index file

---
+++ notification-service/src/handlers/emailHandler.ts
@@ -0,0 +1,5 @@
+import { SQSEvent } from "aws-lambda";
+
+export const CustomerEmailHandler = async (event: SQSEvent) => {
+	return {};
+};
---
+++ notification-service/src/handlers/otpHandler.ts
@@ -0,0 +1,5 @@
+import { SQSEvent } from "aws-lambda";
+
+export const CustomerOTPHandler = async (event: SQSEvent) => {
+	return {};
+};

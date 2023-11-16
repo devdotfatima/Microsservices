4e198738cf8cfc4ba3d5612445efbe2a6f37ee89
Fatima Ali <fatimaaliforwork@gmail.com>
2023-11-16 18:53:39 +0500

    creates handler for sending OTP through twilio

--- notification-service/src/handlers/otpHandler.ts
+++ notification-service/src/handlers/otpHandler.ts
@@ -1,5 +1,32 @@
 import { SQSEvent } from "aws-lambda";
+import { plainToClass } from "class-transformer";
+import { OTPInput } from "../dto/otp-input";
+import { AppValidationError } from "../utility/errors";
+import { SendVerificationCode } from "../utility/sms";
 
 export const CustomerOTPHandler = async (event: SQSEvent) => {
-	return {};
+	const response: Record<string, unknown>[] = [];
+
+	const promisses = event.Records.map(async (record) => {
+		const input = plainToClass(OTPInput, JSON.parse(record.body));
+
+		const errors = await AppValidationError(input);
+
+		console.log("ERRORS: ", JSON.stringify(errors));
+
+		if (!errors) {
+			const { phone, code } = input;
+			await SendVerificationCode(Number(code), phone.trim());
+		} else {
+			response.push({ error: JSON.stringify(errors) });
+		}
+	});
+	await Promise.all(promisses);
+
+	console.log("SQS response:", response);
+
+	return;
+	{
+		response;
+	}
 };
